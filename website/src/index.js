a6bc1887bb1696e362ad5ef79a77f2759bc1de23
Thomas Field <thomasrgfield@gmail.com>
2020-06-01 17:40:44 +0100

    Add existing file

--- website/src/index.js
+++ website/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/utilities.css";
+import "./css/style.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <body>
+    <header className="hero">
+      <div id="navbar" className="navbar top">
+        <h1 className="logo">
+          <span className="text-primary">
+            <i class="fas fa-car"></i> Southwest
+          </span>
+          Remapping
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#cases">Services</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <div className="content">
+        <h1>Engineering for Performance Excellence </h1>
+        <p>We provide world class automotive performance </p>
+        <a href="#about" className="btn">
+          <i class="fas fa-chevron-right"></i> Read More
+        </a>
+      </div>
+    </header>
+  </body>,
+
+  document.getElementById("root")
+);
